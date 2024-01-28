6d4b2384d208b2eee61d979b2bba4b76e8852a32
Mohamed Adel <Mohamed.Adel.Mohamed.Ali@ibm.com>
2024-01-28 18:25:05 +0200

    remove commented code

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,10 +1,4 @@
 import * as mongoose from "mongoose";
-import * as bcrypt from "bcrypt";
-import * as jwt from "jsonwebtoken";
-import { ConfigService } from "@nestjs/config";
-import { ConfigKey } from "@/config/config";
-
-const configService = new ConfigService();
 
 export const UserSchema = new mongoose.Schema(
   {
@@ -15,31 +9,3 @@ export const UserSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
-
-// type HookNextFunction = (err?: Error) => void;
-
-// UserSchema.pre("save", async function (next: HookNextFunction) {
-//   try {
-//     if (!this.isModified("password")) {
-//       return next();
-//     }
-
-//     const { saltRounds } = configService.get(ConfigKey.Bcrypt);
-//     const hashed = bcrypt.hashSync(this["password"], bcrypt.genSaltSync(saltRounds));
-//     this["password"] = hashed;
-//     return next();
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
-
-// UserSchema.methods.generateAuthToken = function () {
-//   const { jwtExpiresIn, jwtSecretKey } = configService.get(ConfigKey.Jwt);
-//   return jwt.sign({ id: this._id, name: this["name"], username: this["username"] }, jwtSecretKey, { expiresIn: jwtExpiresIn });
-// };
-
-// UserSchema.methods.validatePassword = function (password: string): Boolean {
-//   return bcrypt.compareSync(password, this["password"]);
-// };
-
-// export const UserModel = mongoose.model("user", UserSchema);
