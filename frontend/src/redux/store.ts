1842e3b848e2e0129a85352eb47930fc68dbb77f
Mohamed Adel <Mohamed.Adel.Mohamed.Ali@ibm.com>
2024-02-12 20:05:58 +0200

    add logging redux middleware

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,12 +1,27 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import AuthReducer from "./auth/auth-slice";
 
+// Custom middleware function for logging actions and states
+const loggingMiddleware = (store: any) => (next: any) => (action: any) => {
+  const prevState = store.getState();
+
+  const result = next(action);
+  const nextState = store.getState();
+  console.log({
+    Action: action.type,
+    State_before_dispatch: prevState,
+    State_after_dispatch: nextState
+  });
+  return result;
+};
+
+// Redux store with combined reducers and middleware
 export const store = configureStore({
-  reducer: {
-    auth: AuthReducer
-  }
+  reducer: { auth: AuthReducer },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loggingMiddleware)
 });
 
+// Types for dispatch, state, and thunk actions
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
